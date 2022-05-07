dfbe8738ee01b9cc47ea37d56bcfeac3e061459f
hthomp15 <hailethomp@gmail.com>
2022-05-07 15:43:25 -0400

    create pizza api, (model, controller, and routes): connect to front end page

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -51,6 +51,22 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+  fetch('/api/pizzas', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+  }).then(res => res.json())
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Pizza Created Successfully!');
+    })
+    .catch(err => {
+      console.log(err);
+      alert('Pizza Creation Failed!');
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
